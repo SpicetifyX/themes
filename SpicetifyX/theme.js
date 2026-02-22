1d088b364e6d6ab0a0dc59cad97cd653a2d1dce0
Jeffrey Reij <contact@jreij.info>
2026-02-22 13:28:14 +0100

    Update theme.js

--- SpicetifyX/theme.js
+++ SpicetifyX/theme.js
@@ -1,6 +1,13 @@
 (async () => {
-    console.log(`Loaded SpicetifyX theme`)
-    await new Promise((resolve) => setTimeout(resolve, 2500))
-    const el = document.querySelector('[aria-label="History"]')
-    el.remove()
+  console.log(`Loaded SpicetifyX theme`);
+
+  let el;
+  while (!el) {
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    el = document.querySelector('[aria-label="Lyrics"]');
+    console.log(el);
+  }
+  if (Spicetify.Config.extensions.includes("spicy-lyrics.mjs")) {
+    el.remove();
+  }
 })();
